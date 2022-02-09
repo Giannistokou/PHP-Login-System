4d6654e83dd5e6e18c0144c519ee771d8cee8bff
unknown <yiannistokou@gmail.com>
2022-02-09 20:55:20 +0100

    Added config; added login2/reg pages; added footer

---
+++ inc/config.php
@@ -0,0 +1,8 @@
+<?php
+    // if there is no _config_, do not load this file
+    if(!defined('_CONFIG_')) {
+        exit('You do not have a config file');
+    }
+    define('ALLOW_FOOTER', true);
+    // our config is below
+?>
---
+++ register.php
@@ -0,0 +1,51 @@
+<?php
+//allow the config
+define('_CONFIG_', true);
+//Require config
+require_once "inc/config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Page Title</title>
+
+    <base href="/" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.11.1/dist/css/uikit.min.css" />
+</head>
+<body>
+
+<div class="uk-section uk-container">
+    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+        <form class="uk-form-stacked js-register">
+            <h2>Register</h2>
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Email</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email">
+                </div>
+            </div>
+
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Password</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password">
+                </div>
+            </div>
+
+            <div class="uk-margin">
+                <button class="uk-button uk-button-default" type="submit">Register</button>
+            </div>
+
+
+        </form>
+    </div>
+</div>
+<?php require_once "inc/footer.php"; ?>
+
+
+
+</body>
+</html>
+
+
